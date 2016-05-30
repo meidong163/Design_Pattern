08bb797358c2ac08b11a42e0e67bb436d4b616a5
kuaichuan <meidong@meidongMacBook-Air.local>
2016-05-30 23:26:35 +0800

    init Decorater Pattern

---
+++ 装饰者模式（Decorator Pattern）/Decorater_Pattern/Decorater_Pattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Decorater_Pattern
+//
+//  Created by 没懂 on 16/5/30.
+//  Copyright © 2016年 com.comelet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
